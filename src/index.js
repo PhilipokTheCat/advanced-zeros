14a2aa1f118afc7ec989dfbead303f6fcb3f9440
unknown <philipok4596@gmail.com>
2018-02-28 01:55:35 +0300

    advanced-zeros/feature: implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zeroesNumber = number, tempBase = base, power, count, tempNumber;
+  for (let i = 2; i <= base; i++){
+    if (tempBase % i === 0){
+      power = 0;
+      while (tempBase % i === 0) {
+        power++;
+        tempBase /= i;
+      }
+      count = 0;
+      tempNumber = number;
+      while (tempNumber / i > 1){
+        count += Math.floor(tempNumber/i);
+        tempNumber /= i;
+      }
+      zeroesNumber = Math.min(zeroesNumber, Math.floor(count / power));
+    }
+  }
+  return zeroesNumber;
 }
